844066892ff2338f31a2c749c8e0b8d53947bbe1
daddynmr <111244865+daddynmr@users.noreply.github.com>
2024-08-02 23:09:31 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,93 +1,269 @@
-require('dotenv').config();
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const fs = require('fs');
-const config = require('./config');
-const { DisTube } = require('distube');
-const { SpotifyPlugin } = require('@distube/spotify');
-const { YtDlpPlugin } = require('@distube/yt-dlp');
-const { SoundCloudPlugin } = require('@distube/soundcloud');
-const { DeezerPlugin } = require('@distube/deezer');
-const { YouTubePlugin } = require('@distube/youtube');
-const { DirectLinkPlugin } = require('@distube/direct-link');
-const path = require('path');
+import asyncio
+import os
+import discord
+import requests
+import youtube_dl
+from discord.ext import commands
+from dotenv import load_dotenv
 
-const client = new Client({
-    intents: [
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildVoiceStates,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-    ],
-  });
-  
-client.commands = new Collection();
-client.config = config;
+import utilities
 
-fs.readdirSync('./commands').forEach(file => {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
-});
+load_dotenv()
+# Save your discord token in a .env file and link it here.
+token = os.getenv('discordToken')
 
-const eventFiles = fs.readdirSync(path.join(__dirname, 'events')).filter(file => file.endsWith('.js'));
+# Set the bot intents accordingly to be able to read info about guild members etc.
+intents = discord.Intents(messages=True, guilds=True, members=True, message_content=True, presences=True)
 
-eventFiles.forEach(file => {
-  const event = require(`./events/${file}`);
-  const eventName = file.split('.')[0];
-  
-  if (typeof event === 'function') {
-    client.on(eventName, event.bind(null, client));
-  } else {
-    console.error(`Event handler in ${file} is not a function.`, event);
-  }
-});
+# Change '/' to whatever prefix you want to use to call the bot on discord.
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-client.distube = new DisTube(client, {
-    plugins: [
-        new YouTubePlugin(),     // Supports YouTube
-        new SpotifyPlugin(),     // Supports Spotify
-        new SoundCloudPlugin(),  // Supports SoundCloud
-        new DeezerPlugin(),      // Supports Deezer
-        new DirectLinkPlugin(),  // Supports direct audio links
-        new YtDlpPlugin(),       // Supports 700+ sites including YouTube
-    ],
-});
+# YouTube is a bitch and tries to disconnect our bot from its servers. Use this to reconnect instantly.
+# (Because of this disconnect/reconnect cycle, sometimes you will listen a sudden and brief stop)
+FFMPEG_OPTIONS = {
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
-require('./events/distubeEvents')(client);
-
-const express = require("express");
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-    const imagePath = path.join(__dirname, 'index.html');
-    res.sendFile(imagePath);
-});
-app.listen(port, () => {
-    console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
-});
+# List with all the sessions currently active.
+# TODO: Terminate season after X minutes have passed without interaction.
+sessions = []
 
 
-client.login(process.env.TOKEN);
+def check_session(ctx):
+    """
+    Checks if there is a session with the same characteristics (guild and channel) as ctx param.
 
-module.exports = client;
+    :param ctx: discord.ext.commands.Context
 
-
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
+    :return: session()
+    """
+    if len(sessions) > 0:
+        for i in sessions:
+            if i.guild == ctx.guild and i.channel == ctx.author.voice.channel:
+                return i
+        session = utilities.Session(
+            ctx.guild, ctx.author.voice.channel, id=len(sessions))
+        sessions.append(session)
+        return session
+    else:
+        session = utilities.Session(ctx.guild, ctx.author.voice.channel, id=0)
+        sessions.append(session)
+        return session
 
 
-*/
+def prepare_continue_queue(ctx):
+    """
+    Used to call next song in queue.
+
+    Because lambda functions cannot call async functions, I found this workaround in discord's api documentation
+    to let me continue playing the queue when the current song ends.
+
+    :param ctx: discord.ext.commands.Context
+    :return: None
+    """
+    fut = asyncio.run_coroutine_threadsafe(continue_queue(ctx), bot.loop)
+    try:
+        fut.result()
+    except Exception as e:
+        print(e)
+
+
+async def continue_queue(ctx):
+    """
+    Check if there is a next in queue then proceeds to play the next song in queue.
+
+    As you can see, in this method we create a recursive loop using the prepare_continue_queue to make sure we pass
+    through all songs in queue without any mistakes or interaction.
+
+    :param ctx: discord.ext.commands.Context
+    :return: None
+    """
+    session = check_session(ctx)
+    if not session.q.theres_next():
+        await ctx.send("Acabou a queue, brother.")
+        return
+
+    session.q.next()
+
+    voice = discord.utils.get(bot.voice_clients, guild=session.guild)
+    source = await discord.FFmpegOpusAudio.from_probe(session.q.current_music.url, **FFMPEG_OPTIONS)
+
+    if voice.is_playing():
+        voice.stop()
+
+    voice.play(source, after=lambda e: prepare_continue_queue(ctx))
+    await ctx.send(session.q.current_music.thumb)
+    await ctx.send(f"Tocando agora: {session.q.current_music.title}")
+
+
+@bot.command(name='play')
+async def play(ctx, *, arg):
+    """
+    Checks where the command's author is, searches for the music required, joins the same channel as the command's
+    author and then plays the audio directly from YouTube.
+
+    :param ctx: discord.ext.commands.Context
+    :param arg: str
+        arg can be url to video on YouTube or just as you would search it normally.
+    :return: None
+    """
+    try:
+        voice_channel = ctx.author.voice.channel
+
+    # If command's author isn't connected, return.
+    except AttributeError as e:
+        print(e)
+        await ctx.send("Tu não tá conectado num canal de voz, burro")
+        return
+
+    # Finds author's session.
+    session = check_session(ctx)
+
+    # Searches for the video
+    with youtube_dl.YoutubeDL({'format': 'bestaudio', 'noplaylist': 'True'}) as ydl:
+        try:
+            requests.get(arg)
+        except Exception as e:
+            print(e)
+            info = ydl.extract_info(f"ytsearch:{arg}", download=False)[
+                'entries'][0]
+        else:
+            info = ydl.extract_info(arg, download=False)
+
+    url = info['formats'][0]['url']
+    thumb = info['thumbnails'][0]['url']
+    title = info['title']
+
+    session.q.enqueue(title, url, thumb)
+
+    # Finds an available voice client for the bot.
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if not voice:
+        await voice_channel.connect()
+        voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+
+    # If it is already playing something, adds to the queue
+    if voice.is_playing():
+        await ctx.send(thumb)
+        await ctx.send(f"Adicionado à queue: {title}")
+        return
+    else:
+        await ctx.send(thumb)
+        await ctx.send(f"Tocando agora: {title}")
+
+        # Guarantees that the requested music is the current music.
+        session.q.set_last_as_current()
+
+        source = await discord.FFmpegOpusAudio.from_probe(url, **FFMPEG_OPTIONS)
+        voice.play(source, after=lambda ee: prepare_continue_queue(ctx))
+
+
+@bot.command(name='next', aliases=['skip'])
+async def skip(ctx):
+    """
+    Skips the current song, playing the next one in queue if there is one.
+
+    :param ctx: discord.ext.commands.Context
+    :return: None
+    """
+    # Finds author's session.
+    session = check_session(ctx)
+    # If there isn't any song to be played next, return.
+    if not session.q.theres_next():
+        await ctx.send("Não tem porra nenhuma na fila, mangolão")
+        return
+
+    # Finds an available voice client for the bot.
+    voice = discord.utils.get(bot.voice_clients, guild=session.guild)
+
+    # If it is playing something, stops it. This works because of the "after" argument when calling voice.play as it is
+    # a recursive loop and the current song is already going to play the next song when it stops.
+    if voice.is_playing():
+        voice.stop()
+        return
+    else:
+        # If nothing is playing, finds the next song and starts playing it.
+        session.q.next()
+        source = await discord.FFmpegOpusAudio.from_probe(session.q.current_music.url, **FFMPEG_OPTIONS)
+        voice.play(source, after=lambda e: prepare_continue_queue(ctx))
+        return
+
+
+@bot.command(name='print')
+async def print_info(ctx):
+    """
+    A debug command to find session id, what is current playing and what is on the queue.
+    :param ctx: discord.ext.commands.Context
+    :return: None
+    """
+    session = check_session(ctx)
+    await ctx.send(f"Session ID: {session.id}")
+    await ctx.send(f"Música atual: {session.q.current_music.title}")
+    queue = [q[0] for q in session.q.queue]
+    await ctx.send(f"Queue: {queue}")
+
+
+@bot.command(name='leave')
+async def leave(ctx):
+    """
+    If bot is connected to a voice channel, it leaves it.
+
+    :param ctx: discord.ext.commands.Context
+    :return: None
+    """
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_connected:
+        check_session(ctx).q.clear_queue()
+        await voice.disconnect()
+    else:
+        await ctx.send("Bot not connect, so it can't leave.")
+
+
+@bot.command(name='pause')
+async def pause(ctx):
+    """
+    If playing audio, pause it.
+
+    :param ctx: discord.ext.commands.Context
+    :return: None
+    """
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Não ta tocando porra nenhuma mlk va ser burro no inferno")
+
+
+@bot.command(name='resume')
+async def resume(ctx):
+    """
+    If audio is paused, resumes playing it.
+
+    :param ctx: discord.ext.commands.Context
+    :return: None
+    """
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_paused:
+        voice.resume()
+    else:
+        await ctx.send("Música já ta pausada, mangolao")
+
+
+@bot.command(name='stop')
+async def stop(ctx):
+    """
+    Stops playing audio and clears the session's queue.
+
+    :param ctx: discord.ext.commands.Context
+    :return: None
+    """
+    session = check_session(ctx)
+    voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+    if voice.is_playing:
+        voice.stop()
+        session.q.clear_queue()
+    else:
+        await ctx.send("Não tem nada tocando ô abobado.")
+
+
+# Runs bot's loop.
+bot.run(token)
